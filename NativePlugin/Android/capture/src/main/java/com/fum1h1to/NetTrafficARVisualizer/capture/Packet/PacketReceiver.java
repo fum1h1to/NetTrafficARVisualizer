b1f6720a381c20b2146c43f36d127fe686fcb5e5
fum1h1to <75571344+fum1h1to@users.noreply.github.com>
2023-11-23 18:20:32 +0900

    PacketReceiverの実装

--- NativePlugin/Android/capture/src/main/java/com/fum1h1to/NetTrafficARVisualizer/capture/Packet/PacketReceiver.java
+++ NativePlugin/Android/capture/src/main/java/com/fum1h1to/NetTrafficARVisualizer/capture/Packet/PacketReceiver.java
@@ -7,6 +7,8 @@ import com.fum1h1to.NetTrafficARVisualizer.capture.core.protocol.tcpip.Packet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PacketReceiver implements Runnable {
     private static final String TAG = "PacketReceiver";
@@ -18,21 +20,16 @@ public class PacketReceiver implements Runnable {
 
     @Override
     public void run() {
-        try {
-            while(!Thread.interrupted()) {
-                // 一秒間隔でpacketをまとめる
-                Packet packet = PacketRawDataQueue.queue.take();
+        // 1秒間隔でpacketを取り入れる
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
                 List<Packet> packetRaws = new ArrayList<>();
+                PacketRawDataQueue.queue.drainTo(packetRaws);
 
-                // まとめたものをpacketConverterへ
                 mPacketConverter.submitPacketRaws(packetRaws);
-
             }
-
-        } catch(InterruptedException e){
-            Log.i(TAG, "CaptureThread finish");
-        } catch (Exception e) {
-            Log.i(TAG, "capture fail", e);
-        }
+        };
+        timer.scheduleAtFixedRate(task,0,1000);
     }
 }
